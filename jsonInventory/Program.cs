24822c258af5e2ddacb95a6a72e3e51a6a973c43
Shivani21-hude <shivani21hude@gmail.com>
2022-12-29 20:28:55 +0530

    [Shivani]-Json inventory data management of rice,pulses and wheats

---
+++ jsonInventory/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to JSON Inventory Data Management \n");
+            string inventoryFilePath = @"E:\\RFP\JSONInventory\\JSONsample\\Inventory.json";
+            JSOnMain jSOnMain = new JSOnMain();
+            jSOnMain.ReadJsonFile(inventoryFilePath);
+        }
+    }
+}
