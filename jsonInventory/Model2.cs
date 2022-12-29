dd863c8b1b99956f8af4d61343b250cbcda89d67
Shivani21-hude <shivani21hude@gmail.com>
2022-12-29 22:06:18 +0530

    [Shivani]-Extent data management to create inventory management

---
+++ jsonInventory/Model2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonInventory
+{
+    internal class Model2
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+        public List<Model2> TypeofRice { get; set; }
+        public List<Model2> TpeofPulses { get; set; }
+        public List<Model2> TypeofWheat { get; set; }
+        public static string ManagementfilePath { get; private set; }
+    }
+}
+
+
+            
+
